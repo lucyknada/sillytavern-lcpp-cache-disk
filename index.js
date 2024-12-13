e41708b8aaa13696d1f3041226fe8f1452768659
lucy <154630366+lucyknada@users.noreply.github.com>
2024-12-13 04:07:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// import { getContext } from "../../../extensions.js";
+import { eventSource, event_types } from "../../../../script.js";
+import { getContext } from "../../../extensions.js";
 
 // settings
 
@@ -6,14 +7,23 @@ const LCPP_SERVER = "127.0.0.1:8080"
 
 // dont touch anything below that
 
-function hashCode(s) {
-    return s.split("").reduce(function (a, b) {
-        a = ((a << 5) - a) + b.charCodeAt(0);
-        return a & a;
-    }, 0);
-}
+const hashCode = (str, seed = 0) => {
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for (let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
 
 const saveChat = async () => {
+    console.log("shiee", hashCode(getContext().chatId))
     await fetch(`http://${LCPP_SERVER}/slots/0?action=save`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -33,12 +43,15 @@ const loadChat = async (id) => {
 }
 
 eventSource.on(event_types.CHAT_CHANGED, (id) => {
-    (async () => {
+    console.log("shiee", "chat_changed")
+    !(async () => {
         loadChat(id);
-    })
+    })()
 })
+
 eventSource.on(event_types.MESSAGE_RECEIVED, () => {
-    (async () => {
+    console.log("shiee", "message_received")
+    !(async () => {
         saveChat();
-    })
+    })()
 })
